a0742f935c739a6ff45673f63249a726854356f4
johnthagen <johnthagen@gmail.com>
2016-08-23 20:26:31 -0400

    Piston hello world example.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate piston_window;
+
+use piston_window::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut window: PistonWindow =
+        WindowSettings::new("Hello Piston!", [640, 480])
+        .exit_on_esc(true).build().unwrap();
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |c, g| {
+            clear([1.0; 4], g);
+            rectangle([1.0, 0.0, 0.0, 1.0], // red
+                      [0.0, 0.0, 100.0, 100.0],
+                      c.transform, g);
+        });
+    }
 }
