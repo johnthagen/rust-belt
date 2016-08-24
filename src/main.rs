3194047206caf0ef4a37237e75291d63a4f64593
johnthagen <johnthagen@gmail.com>
2016-08-24 16:13:55 -0400

    Reflow line.

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ extern crate piston_window;
 use piston_window::*;
 
 fn main() {
-    let mut window: PistonWindow =
-        WindowSettings::new("Hello Piston!", [640, 480])
+    let mut window: PistonWindow = WindowSettings::new("Hello Piston!", [640, 480])
         .exit_on_esc(true).build().unwrap();
     while let Some(e) = window.next() {
         window.draw_2d(&e, |c, g| {
