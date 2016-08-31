c7feb2318a53f7d84e4cf2731d48cb7af6e3afad
johnthagen <johnthagen@gmail.com>
2016-08-31 16:45:01 -0400

    Expand source to make it easier to read.

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,18 @@ extern crate piston_window;
 use piston_window::{clear, PistonWindow, rectangle, WindowSettings};
 
 fn main() {
-    let mut window: PistonWindow = WindowSettings::new("Hello Piston!", [640, 480])
-        .exit_on_esc(true).build().unwrap();
-    while let Some(e) = window.next() {
-        window.draw_2d(&e, |c, g| {
-            clear([1.0; 4], g);
-            rectangle([1.0, 0.0, 0.0, 1.0], // red
-                      [0.0, 0.0, 100.0, 100.0],
-                      c.transform, g);
-        });
+    let mut window: PistonWindow = WindowSettings::new("Rust Belt", [640, 480])
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    while let Some(event) = window.next() {
+        window.draw_2d(&event,
+                       |c, g| {
+                           clear([1.0; 4], g);
+                           rectangle([1.0, 0.0, 0.0, 1.0], // red
+                                     [0.0, 0.0, 100.0, 100.0],
+                                     c.transform, g);
+                       });
     }
 }
