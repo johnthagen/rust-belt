6e85be01f20a21461087f922f3ec1826d3033c50
johnthagen <johnthagen@gmail.com>
2016-09-02 14:43:54 -0400

    Upgrade to the hello_world piston_window example.  Update to latest piston_window.

--- src/main.rs
+++ src/main.rs
@@ -2,65 +2,48 @@ extern crate piston_window;
 
 use piston_window::*;
 
-pub struct App {
-    rotation: f64   // Rotation for the square.
-}
-
-impl App {
-    fn render(&mut self, e: &PistonWindow, args: &RenderArgs) {
-
-        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-
-        let square = rectangle::square(0.0, 0.0, 50.0);
-        let rotation = self.rotation;
-        let (x, y) = ((args.width / 2) as f64,
-                      (args.height / 2) as f64);
-
-        e.draw_2d(|c, gl| {
-
-            // Clear the screen.
-            clear(GREEN, gl);
-
-            let transform = c.transform.trans(x, y)
-                                       .rot_rad(rotation)
-                                       .trans(-25.0, -25.0);
-
-            // Draw a box rotating around the middle of the screen.
-            rectangle(RED, square, transform, gl);
-        });
-    }
-
-    fn update(&mut self, args: &UpdateArgs) {
-        // Rotate 2 radians per second.
-        self.rotation += 2.0 * args.dt;
-    }
-}
-
 fn main() {
-
-    // Create an Glutin window.
-    let window: PistonWindow = WindowSettings::new(
-            "spinning-square",
-            [200, 200]
-        )
+    let title = "Hello Piston! (press any key to enter inner loop)";
+    let mut window: PistonWindow = WindowSettings::new(title, [640, 480])
         .exit_on_esc(true)
         .build()
-        .unwrap();
+        .unwrap_or_else(|e| { panic!("Failed to build PistonWindow: {}", e) });
 
-    // Create a new game and run it.
-    let mut app = App {
-        rotation: 0.0
-    };
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |c, g| {
+            clear([0.5, 1.0, 0.5, 1.0], g);
+            rectangle([1.0, 0.0, 0.0, 1.0], [50.0, 50.0, 100.0, 100.0], c.transform, g);
+        });
 
-    for e in window {
-
-        if let Some(r) = e.render_args() {
-            app.render(&e,&r);
+        if e.press_args().is_some() {
+            InnerApp {
+                title: "Inner loop (press X to exit inner loop)",
+                exit_button: Button::Keyboard(Key::X),
+            }.run(&mut window);
+            window.set_title(title.into());
         }
+    }
+}
 
-        if let Some(u) = e.update_args() {
-            app.update(&u);
+/// Stores application state of inner event loop.
+pub struct InnerApp {
+    pub title: &'static str,
+    pub exit_button: Button,
+}
+
+impl InnerApp {
+    pub fn run(&mut self, window: &mut PistonWindow) {
+        window.set_title(self.title.into());
+        while let Some(e) = window.next() {
+            window.draw_2d(&e, |c, g| {
+                clear([0.5, 0.5, 1.0, 1.0], g);
+                ellipse([1.0, 0.0, 0.0, 1.0], [50.0, 50.0, 100.0, 100.0], c.transform, g);
+            });
+            if let Some(button) = e.press_args() {
+                if button == self.exit_button {
+                    break;
+                }
+            }
         }
     }
 }
