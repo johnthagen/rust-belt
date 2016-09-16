0f07f46387f1e8972e97dba9c0e7a4ce1a449ea1
johnthagen <johnthagen@gmail.com>
2016-09-16 19:14:44 -0400

    Add initial story screen.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate piston_window;
 extern crate find_folder;
 
-use piston_window::{Button, clear, Glyphs, Key, PressEvent, PistonWindow, polygon, text,
+use piston_window::{Button, clear, Glyphs, Key, PressEvent, PistonWindow, polygon, text, types,
     Transformed, WindowSettings};
 
 mod color {
@@ -11,6 +11,7 @@ mod color {
     pub const WHITE: types::Color = [1.0, 1.0, 1.0, 1.0];
     pub const CYAN: types::Color = [0.0, 1.0, 1.0, 1.0];
     pub const YELLOW: types::Color = [1.0, 1.0, 0.0, 1.0];
+    pub const MAGENTA: types::Color = [1.0, 0.0, 1.0, 1.0];
 }
 
 const GAME_TITLE: &'static str = "Rust Belt";
@@ -45,13 +46,14 @@ impl Menu {
     fn run(&mut self, mut window: &mut PistonWindow) {
         let assets = find_folder::Search::ParentsThenKids(3, 3)
             .for_folder("assets").unwrap();
-        println!("{:?}", assets);
-        let ref font = assets.join("FiraSans-Regular.ttf");
+        let ref font_file = assets.join("FiraSans-Regular.ttf");
         let factory = window.factory.clone();
-        let mut glyphs = Glyphs::new(font, factory).unwrap();
+        let mut glyph_cache = Glyphs::new(font_file, factory).unwrap();
 
         while let Some(event) = window.next() {
             const MENU_ALIGN: f64 = ((GAME_WINDOW_WIDTH / 2) - 120) as f64;
+            const STARTING_LINE_OFFSET: f64 = 280.0;
+            const NEW_LINE_OFFSET: f64 = 40.0;
 
             // TODO: Can this be done better with 'if let' ?
             let mut play_color = color::WHITE;
@@ -69,30 +71,33 @@ impl Menu {
                                text(color::WHITE,
                                     72,
                                     GAME_TITLE,
-                                    &mut glyphs,
+                                    &mut glyph_cache,
                                     context.transform
-                                        .trans(MENU_ALIGN, 280.0),
+                                        .trans(MENU_ALIGN, STARTING_LINE_OFFSET),
                                     graphics);
                                text(play_color,
                                     32,
                                     "Play",
-                                    &mut glyphs,
+                                    &mut glyph_cache,
                                     context.transform
-                                        .trans(MENU_ALIGN, 320.0),
+                                        .trans(MENU_ALIGN, STARTING_LINE_OFFSET +
+                                            1.0 * NEW_LINE_OFFSET),
                                     graphics);
                                text(story_color,
                                     32,
                                     "Story",
-                                    &mut glyphs,
+                                    &mut glyph_cache,
                                     context.transform
-                                        .trans(MENU_ALIGN, 360.0),
+                                        .trans(MENU_ALIGN, STARTING_LINE_OFFSET +
+                                            2.0 * NEW_LINE_OFFSET),
                                     graphics);
                                text(exit_color,
                                     32,
                                     "Exit",
-                                    &mut glyphs,
+                                    &mut glyph_cache,
                                     context.transform
-                                        .trans(MENU_ALIGN, 400.0),
+                                        .trans(MENU_ALIGN, STARTING_LINE_OFFSET +
+                                            3.0 * NEW_LINE_OFFSET),
                                     graphics);
                            });
 
@@ -120,12 +125,116 @@ impl Menu {
                                     rotation: 0.0,
                                 }.run(&mut window);
                             }
+                            MenuSelection::Story => {
+                                run_story(&mut window, font_file);
+                            }
                             MenuSelection::Exit => { break }
-                            _ => {}
                         }
                     }
                     _ => {}
                 }
+            }
+        }
+    }
+}
+
+fn run_story(window: &mut PistonWindow, font_file: &std::path::PathBuf) {
+    // TODO: Find a way to pass in the GlyphCache directly instead of the path to the font file.
+    // GlyphCache is located in piston2d-gfx_graphics.
+    let mut glyph_cache = Glyphs::new(font_file, window.factory.clone()).unwrap();
+    while let Some(event) = window.next() {
+        // TODO: Create a vector of tuples that map message and color and iterate.
+        const LEFT_INDENT: f64 = 10.0;
+        const NARRATOR_COLOR: types::Color = color::WHITE;
+        const KARA_COLOR: types::Color = color::MAGENTA;
+        const JACK_COLOR: types::Color = color::CYAN;
+        const STARTING_LINE_OFFSET: f64 = 30.0;
+        const NEW_LINE_OFFSET: f64 = 40.0;
+        const FONT_SIZE: u32 = 32;
+
+        window.draw_2d(&event,
+                       |context, graphics| {
+                           clear(color::BLACK, graphics);
+                           text(NARRATOR_COLOR,
+                                FONT_SIZE,
+                                "The stars snap back into place, jolting your neck forward.",
+                                &mut glyph_cache,
+                                context.transform
+                                    .trans(LEFT_INDENT, STARTING_LINE_OFFSET),
+                                graphics);
+                           text(NARRATOR_COLOR,
+                                FONT_SIZE,
+                                "Panicking, you check your ship’s readouts. \
+                                This can’t be the right system.",
+                                &mut glyph_cache,
+                                context.transform
+                                    .trans(LEFT_INDENT, STARTING_LINE_OFFSET +
+                                        1.0 * NEW_LINE_OFFSET),
+                                graphics);
+                           text(KARA_COLOR,
+                                FONT_SIZE,
+                                "---day, can --- read me?",
+                                &mut glyph_cache,
+                                context.transform
+                                    .trans(LEFT_INDENT, STARTING_LINE_OFFSET +
+                                        2.0 * NEW_LINE_OFFSET),
+                                graphics);
+                           text(JACK_COLOR,
+                                FONT_SIZE,
+                                "This is Delta-Six, what is your situation?",
+                                &mut glyph_cache,
+                                context.transform
+                                    .trans(LEFT_INDENT, STARTING_LINE_OFFSET +
+                                        3.0 * NEW_LINE_OFFSET),
+                                graphics);
+                           text(NARRATOR_COLOR,
+                                FONT_SIZE,
+                                "A piece of twisted metal screeches off your ship’s shields.",
+                                &mut glyph_cache,
+                                context.transform
+                                    .trans(LEFT_INDENT, STARTING_LINE_OFFSET +
+                                        4.0 * NEW_LINE_OFFSET),
+                                graphics);
+                           text(KARA_COLOR,
+                                FONT_SIZE,
+                                "Jack ---? Jack is --- you?",
+                                &mut glyph_cache,
+                                context.transform
+                                    .trans(LEFT_INDENT, STARTING_LINE_OFFSET +
+                                        5.0 * NEW_LINE_OFFSET),
+                                graphics);
+                           text(JACK_COLOR,
+                                FONT_SIZE,
+                                "Kara, what happened here? Where’s the fleet?",
+                                &mut glyph_cache,
+                                context.transform
+                                    .trans(LEFT_INDENT, STARTING_LINE_OFFSET +
+                                        6.0 * NEW_LINE_OFFSET),
+                                graphics);
+                           text(NARRATOR_COLOR,
+                                FONT_SIZE,
+                                "A lifeless expanse of debris is all that surrounds you in every \
+                                 direction.",
+                                &mut glyph_cache,
+                                context.transform
+                                    .trans(LEFT_INDENT, STARTING_LINE_OFFSET +
+                                        7.0 * NEW_LINE_OFFSET),
+                                graphics);
+                           text(KARA_COLOR,
+                                FONT_SIZE,
+                                "--- ambush. --- --- tried to --- long range transmitter --- \
+                                --- warn ---",
+                                &mut glyph_cache,
+                                context.transform
+                                    .trans(LEFT_INDENT, STARTING_LINE_OFFSET +
+                                        8.0 * NEW_LINE_OFFSET),
+                                graphics);
+                       });
+
+        if let Some(button) = event.press_args() {
+            match button {
+                Button::Keyboard(Key::Space) => { break }
+                _ => {}
             }
         }
     }
@@ -142,7 +251,7 @@ const SHIP: &'static [[f64; 2]] = &[
 
 struct Position {
     x: f64,
-    y: f64
+    y: f64,
 }
 
 /// Stores Game state.
