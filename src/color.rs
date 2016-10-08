d9cbd76775b4a250cab7395d73ff1027967a3755
johnthagen <johnthagen@gmail.com>
2016-10-08 15:57:02 -0400

    Refactor color and story into modules.

---
+++ src/color.rs
@@ -0,0 +1,9 @@
+//! Defines common, reusable colors.
+
+use piston_window::{types};
+
+pub const BLACK: types::Color = [0.0, 0.0, 0.0, 1.0];
+pub const WHITE: types::Color = [1.0, 1.0, 1.0, 1.0];
+pub const CYAN: types::Color = [0.0, 1.0, 1.0, 1.0];
+pub const YELLOW: types::Color = [1.0, 1.0, 0.0, 1.0];
+pub const MAGENTA: types::Color = [1.0, 0.0, 1.0, 1.0];
