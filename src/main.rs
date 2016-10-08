2a7e3c76cb7592cde544e46e7b32fc52d0aa5205
johnthagen <johnthagen@gmail.com>
2016-10-08 16:11:27 -0400

    Refactor menu into separate module.

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ extern crate music;
 
 mod color;
 mod game;
+mod menu;
 mod story;
 
-use piston_window::{Button, clear, Glyphs, Key, PressEvent, PistonWindow, text, Transformed,
-    WindowSettings};
+use piston_window::{ PistonWindow, WindowSettings};
 
 const GAME_TITLE: &'static str = "Rust Belt";
 const GAME_WINDOW_WIDTH: u32 = 1024;
@@ -31,115 +31,8 @@ fn main() {
         music::bind_file(Music::Action, "./assets/Into the Field.mp3");
         music::play(&Music::Menu, music::Repeat::Forever);
 
-        Menu {
-            menu_selection: MenuSelection::Play,
-        }.run(&mut window);
+        menu::Menu {
+            menu_selection: menu::MenuSelection::Play,
+        }.run(&mut window, GAME_TITLE, GAME_WINDOW_WIDTH);
     });
-}
-
-enum MenuSelection {
-    Play,
-    Story,
-    Exit
-}
-
-/// Stores Menu state.
-struct Menu {
-    menu_selection: MenuSelection,
-}
-
-impl Menu {
-    fn run(&mut self, mut window: &mut PistonWindow) {
-        let assets_folder = find_folder::Search::ParentsThenKids(3, 3)
-            .for_folder("assets").unwrap();
-        let ref font_file = assets_folder.join("FiraSans-Regular.ttf");
-        let factory = window.factory.clone();
-        let mut glyph_cache = Glyphs::new(font_file, factory).unwrap();
-
-        while let Some(event) = window.next() {
-            const MENU_ALIGN: f64 = ((GAME_WINDOW_WIDTH / 2) - 120) as f64;
-            const STARTING_LINE_OFFSET: f64 = 280.0;
-            const NEW_LINE_OFFSET: f64 = 40.0;
-
-            // TODO: Can this be done better with 'if let' ?
-            let mut play_color = color::WHITE;
-            let mut story_color = color::WHITE;
-            let mut exit_color = color::WHITE;
-            match self.menu_selection {
-                MenuSelection::Play => { play_color = color::YELLOW }
-                MenuSelection::Story => { story_color = color::YELLOW }
-                MenuSelection::Exit => { exit_color = color::YELLOW }
-            }
-
-            window.draw_2d(&event,
-                           |context, graphics| {
-                               clear(color::BLACK, graphics);
-                               text(color::WHITE,
-                                    72,
-                                    GAME_TITLE,
-                                    &mut glyph_cache,
-                                    context.transform
-                                        .trans(MENU_ALIGN, STARTING_LINE_OFFSET),
-                                    graphics);
-                               text(play_color,
-                                    32,
-                                    "Play",
-                                    &mut glyph_cache,
-                                    context.transform
-                                        .trans(MENU_ALIGN, STARTING_LINE_OFFSET +
-                                            1.0 * NEW_LINE_OFFSET),
-                                    graphics);
-                               text(story_color,
-                                    32,
-                                    "Story",
-                                    &mut glyph_cache,
-                                    context.transform
-                                        .trans(MENU_ALIGN, STARTING_LINE_OFFSET +
-                                            2.0 * NEW_LINE_OFFSET),
-                                    graphics);
-                               text(exit_color,
-                                    32,
-                                    "Exit",
-                                    &mut glyph_cache,
-                                    context.transform
-                                        .trans(MENU_ALIGN, STARTING_LINE_OFFSET +
-                                            3.0 * NEW_LINE_OFFSET),
-                                    graphics);
-                           });
-
-            if let Some(button) = event.press_args() {
-                match button {
-                    Button::Keyboard(Key::W) => {
-                        match self.menu_selection {
-                            MenuSelection::Play => {}
-                            MenuSelection::Story => { self.menu_selection = MenuSelection::Play }
-                            MenuSelection::Exit => { self.menu_selection = MenuSelection::Story }
-                        }
-                    }
-                    Button::Keyboard(Key::S) => {
-                        match self.menu_selection {
-                            MenuSelection::Play => { self.menu_selection = MenuSelection::Story }
-                            MenuSelection::Story => { self.menu_selection = MenuSelection::Exit }
-                            MenuSelection::Exit => {}
-                        }
-                    }
-                    Button::Keyboard(Key::Space) => {
-                        match self.menu_selection {
-                            MenuSelection::Play => {
-                                game::Game {
-                                    position: game::Position { x: 0.0, y: 0.0 },
-                                    rotation: 0.0,
-                                }.run(&mut window);
-                            }
-                            MenuSelection::Story => {
-                                story::run(&mut window, font_file);
-                            }
-                            MenuSelection::Exit => { break }
-                        }
-                    }
-                    _ => {}
-                }
-            }
-        }
-    }
 }
---
+++ src/menu.rs
@@ -0,0 +1,117 @@
+//! Main menu.
+
+use piston_window::{Button, clear, Glyphs, Key, PressEvent, PistonWindow, text, Transformed};
+
+use find_folder;
+
+use color;
+use game;
+use story;
+
+pub enum MenuSelection {
+    Play,
+    Story,
+    Exit
+}
+
+/// Stores Menu state.
+pub struct Menu {
+    pub menu_selection: MenuSelection,
+}
+
+impl Menu {
+    pub fn run(&mut self, mut window: &mut PistonWindow, game_title: &'static str,
+               window_width: u32) {
+        let assets_folder = find_folder::Search::ParentsThenKids(3, 3)
+            .for_folder("assets").unwrap();
+        let ref font_file = assets_folder.join("FiraSans-Regular.ttf");
+        let factory = window.factory.clone();
+        let mut glyph_cache = Glyphs::new(font_file, factory).unwrap();
+
+        while let Some(event) = window.next() {
+            let menu_align: f64 = ((window_width / 2) - 120) as f64;
+            const STARTING_LINE_OFFSET: f64 = 280.0;
+            const NEW_LINE_OFFSET: f64 = 40.0;
+
+            // TODO: Can this be done better with 'if let' ?
+            let mut play_color = color::WHITE;
+            let mut story_color = color::WHITE;
+            let mut exit_color = color::WHITE;
+            match self.menu_selection {
+                MenuSelection::Play => { play_color = color::YELLOW }
+                MenuSelection::Story => { story_color = color::YELLOW }
+                MenuSelection::Exit => { exit_color = color::YELLOW }
+            }
+
+            window.draw_2d(&event,
+                           |context, graphics| {
+                               clear(color::BLACK, graphics);
+                               text(color::WHITE,
+                                    72,
+                                    game_title,
+                                    &mut glyph_cache,
+                                    context.transform
+                                        .trans(menu_align, STARTING_LINE_OFFSET),
+                                    graphics);
+                               text(play_color,
+                                    32,
+                                    "Play",
+                                    &mut glyph_cache,
+                                    context.transform
+                                        .trans(menu_align, STARTING_LINE_OFFSET +
+                                            1.0 * NEW_LINE_OFFSET),
+                                    graphics);
+                               text(story_color,
+                                    32,
+                                    "Story",
+                                    &mut glyph_cache,
+                                    context.transform
+                                        .trans(menu_align, STARTING_LINE_OFFSET +
+                                            2.0 * NEW_LINE_OFFSET),
+                                    graphics);
+                               text(exit_color,
+                                    32,
+                                    "Exit",
+                                    &mut glyph_cache,
+                                    context.transform
+                                        .trans(menu_align, STARTING_LINE_OFFSET +
+                                            3.0 * NEW_LINE_OFFSET),
+                                    graphics);
+                           });
+
+            if let Some(button) = event.press_args() {
+                match button {
+                    Button::Keyboard(Key::W) => {
+                        match self.menu_selection {
+                            MenuSelection::Play => {}
+                            MenuSelection::Story => { self.menu_selection = MenuSelection::Play }
+                            MenuSelection::Exit => { self.menu_selection = MenuSelection::Story }
+                        }
+                    }
+                    Button::Keyboard(Key::S) => {
+                        match self.menu_selection {
+                            MenuSelection::Play => { self.menu_selection = MenuSelection::Story }
+                            MenuSelection::Story => { self.menu_selection = MenuSelection::Exit }
+                            MenuSelection::Exit => {}
+                        }
+                    }
+                    Button::Keyboard(Key::Space) => {
+                        match self.menu_selection {
+                            MenuSelection::Play => {
+                                game::Game {
+                                    position: game::Position { x: 0.0, y: 0.0 },
+                                    rotation: 0.0,
+                                }.run(&mut window);
+                            }
+                            MenuSelection::Story => {
+                                story::run(&mut window, font_file);
+                            }
+                            MenuSelection::Exit => { break }
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+}
